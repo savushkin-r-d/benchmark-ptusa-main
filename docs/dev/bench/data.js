1784c6cdd056de53eb19d87eee59eb47130e95a5
github-action-benchmark <github@users.noreply.github.com>
2024-12-20 14:06:48 +0000

    add C++ Benchmark (googlecpp) benchmark result for 66f4845eec471cdcce61164ef8eebd6f69018e84

--- docs/dev/bench/data.js
+++ docs/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734703596614,
+  "lastUpdate": 1734703608211,
   "repoUrl": "https://github.com/savushkin-r-d/ptusa_main",
   "entries": {
     "C++ Benchmark": [
@@ -42382,6 +42382,38 @@ window.BENCHMARK_DATA = {
             "value": 108.18487662928086,
             "unit": "us/iter",
             "extra": "iterations: 6598\ncpu: 106.71901333737499 us\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "idzm",
+            "username": "idzm"
+          },
+          "committer": {
+            "name": "idzm",
+            "username": "idzm"
+          },
+          "id": "66f4845eec471cdcce61164ef8eebd6f69018e84",
+          "message": "Fix unused variables",
+          "timestamp": "2024-10-09T12:44:39Z",
+          "url": "https://github.com/savushkin-r-d/ptusa_main/pull/825/commits/66f4845eec471cdcce61164ef8eebd6f69018e84"
+        },
+        "date": 1734703607110,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "write_devices_service/\"no compression\"",
+            "value": 17.931465476522355,
+            "unit": "us/iter",
+            "extra": "iterations: 39379\ncpu: 17.89306704080855 us\nthreads: 1"
+          },
+          {
+            "name": "write_devices_service/\"with compression\"",
+            "value": 108.43661136784593,
+            "unit": "us/iter",
+            "extra": "iterations: 6492\ncpu: 106.90919239063464 us\nthreads: 1"
           }
         ]
       }
