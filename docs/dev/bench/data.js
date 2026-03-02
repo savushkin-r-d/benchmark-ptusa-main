bdac233858cf59319d02aa31ce13be481dd678d9
github-action-benchmark <github@users.noreply.github.com>
2026-03-02 05:55:56 +0000

    add C++ Benchmark (googlecpp) benchmark result for 42e009a885f268a9a2a34f5bfc4ec7b5d946f7ce

--- docs/dev/bench/data.js
+++ docs/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772430887013,
+  "lastUpdate": 1772430956240,
   "repoUrl": "https://github.com/savushkin-r-d/ptusa_main",
   "entries": {
     "C++ Benchmark": [
@@ -87932,6 +87932,40 @@ window.BENCHMARK_DATA = {
             "value": 83.76213817412803,
             "unit": "us/iter",
             "extra": "iterations: 8511\ncpu: 82.78280824814945 us\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "42e009a885f268a9a2a34f5bfc4ec7b5d946f7ce",
+          "message": "Bump actions/download-artifact from 7.0.0 to 8.0.0 (#1242)\n\nBumps [actions/download-artifact](https://github.com/actions/download-artifact) from 7.0.0 to 8.0.0.\n- [Release notes](https://github.com/actions/download-artifact/releases)\n- [Commits](https://github.com/actions/download-artifact/compare/37930b1c2abaa49bbe596cd826c3c89aef350131...70fc10c6e5e1ce46ad2ea6f2b72d43f7d47b13c3)\n\n---\nupdated-dependencies:\n- dependency-name: actions/download-artifact\n  dependency-version: 8.0.0\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-02T05:40:09Z",
+          "url": "https://github.com/savushkin-r-d/ptusa_main/commit/42e009a885f268a9a2a34f5bfc4ec7b5d946f7ce"
+        },
+        "date": 1772430950429,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "write_devices_service/\"no compression\"",
+            "value": 17.38718801992524,
+            "unit": "us/iter",
+            "extra": "iterations: 40150\ncpu: 17.385595491905356 us\nthreads: 1"
+          },
+          {
+            "name": "write_devices_service/\"with compression\"",
+            "value": 113.4465177537872,
+            "unit": "us/iter",
+            "extra": "iterations: 6393\ncpu: 112.36869513530424 us\nthreads: 1"
           }
         ]
       }
